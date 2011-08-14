1580cd19eaad260651c1f35cb2ee8f32e3905a31
bhasker@gmail.com <bhasker@gmail.com@18326ad8-943d-2d9d-a211-8e6e7e412fff>
2011-08-14 04:09:57 +0000

    supports basic spawning and map with grid display

--- trunk/TowerDefense/src/com/towerdefense/Game.java
+++ trunk/TowerDefense/src/com/towerdefense/Game.java
@@ -4,8 +4,13 @@
  */
 package com.towerdefense;
 
-import com.jme3.app.Application;
+import com.jme3.material.Material;
+import com.jme3.math.ColorRGBA;
+import com.jme3.math.Vector3f;
 import com.jme3.renderer.RenderManager;
+import com.jme3.scene.Geometry;
+import com.jme3.scene.debug.Grid;
+import com.jme3.scene.shape.Box;
 
 /**
  *
@@ -13,33 +18,57 @@ import com.jme3.renderer.RenderManager;
  */
 public class Game {
     
-    private Application app;
+    private TDApp app;
     private AIAppState aiState;
-    public Game(Application app) {
+    private Geometry levelFloor;
+    private Geometry levelGrid;
+    public Game(TDApp app) {
         this.app = app;        
     }
     
     public boolean startGame() {
+        System.out.println("Game::startGame()");
         loadLevel();
-        initAIAppState();
         initHud();
-        //Now that initialization of UI is complete we can trigger the ai
-        aiState.setActive(true);
+        initAIAppState();
+        app.getStateManager().attach(aiState);
         return true;
     }
     
     public boolean loadLevel() {
+        System.out.println("Game::loadLevel");
+        //create a green floor for the level.
+        levelGrid = new Geometry("floorGrid",new Grid(11,21,1.0f));
+       
+        Material gridMat = new Material(app.getAssetManager(),"Common/MatDefs/Misc/SolidColor.j3md");
+        gridMat.getAdditionalRenderState().setWireframe(true);
+        gridMat.setColor("m_Color",new ColorRGBA(0.0f,0.0f,0.0f,1.0f));
+        levelGrid.setMaterial(gridMat);
+        levelGrid.center().move(new Vector3f(10.0f,0.1f,5.0f));
+       
+        levelFloor = new Geometry("floor",new Box(Vector3f.ZERO,10.0f,0.0f,5.0f));     
+        Material floorMat = new Material(app.getAssetManager(),"Common/MatDefs/Misc/SolidColor.j3md");
+        floorMat.setColor("m_Color",new ColorRGBA(0.2f,0.6f,0.2f,1.0f));
+        levelFloor.setMaterial(floorMat);
+        levelFloor.center().move(new Vector3f(10.0f,0.0f,5.0f));
+        
+        app.getRootNode().attachChild(levelFloor);
+        app.getRootNode().attachChild(levelGrid);
+        app.getCamera().setLocation(new Vector3f(10.0f,20.0f,5.1f));
+        app.getCamera().lookAt(new Vector3f(10.0f,0.0f,5.0f),Vector3f.UNIT_Y);
+        app.getFlyByCamera().setEnabled(false);
         return true;
     }
     
     private void initAIAppState() {
-        aiState = new AIAppState();
-        //set inactive so that ai doesn't start till initialization is complete
-        aiState.setActive(false);
-        app.getStateManager().attach(aiState);
+        System.out.println("Game::initAIAppState()");
+        aiState = new AIAppState(); 
+        aiState.addSpawnPoint(Vector3f.ZERO);
+        aiState.addTarget(new Vector3f(20,0,10));
     }
     
     private boolean initHud() {
+        System.out.println("Game::initHud()");
         return true;
     }
     
